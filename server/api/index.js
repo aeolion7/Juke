28eb51c15b561821fe1efc9a4ab27f2cc3b0da64
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-18 11:12:32 -0500

    express routing complete

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,27 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const { Album, Artist, Song } = require('../db');
 
 // connect your API routes here!
 
-module.exports = router
+router.get('/albums', async (req, res, next) => {
+  try {
+    const albums = await Album.findAll();
+    res.send(albums);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+router.get('/albums/:albumId', async (req, res, next) => {
+  try {
+    const albumId = req.params.albumId;
+    const thisAlbum = await Album.findById(albumId, {
+      include: [Artist, Song],
+    });
+    res.send(thisAlbum);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+module.exports = router;
